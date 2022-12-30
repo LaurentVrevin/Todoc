035fa7dd4698d48f5699e2e453e4595724b5cd44
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2022-12-30 16:00:07 +0100

    Request DAO ok

--- app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
@@ -1,4 +1,23 @@
 package com.cleanup.todoc.database.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+@Dao
 public interface TaskDao {
+    //Récupère toutes les tasks via projectId
+    @Query("SELECT * FROM Task WHERE projectId =:projectId")
+    LiveData<List<Task>>getTasks(long projectId);
+
+    @Insert
+    long insertTask(Task task);
+
+    @Query("DELETE FROM Task WHERE id =:taskId")
+    int deleteTask(long taskId);
 }
